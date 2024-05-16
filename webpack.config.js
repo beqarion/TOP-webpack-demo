85e4379a6f39f759046b42999d51c662a4db6a6a
Bekarion <beqarioni@yahoo.co.uk>
2024-05-17 00:06:22 +0400

    top-webp-assignmnt-4.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
   output: {
-    filename: "bundle.js",
+    filename: "[name].[contenthash].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   module: {
     rules: [
@@ -29,5 +33,19 @@ module.exports = {
         use: ["xml-loader"],
       },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      title: "title from plugin",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+  },
+  optimization: {
+    runtimeChunk: "single",
   },
 };
